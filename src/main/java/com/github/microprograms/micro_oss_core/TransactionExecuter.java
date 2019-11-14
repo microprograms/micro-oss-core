5996228428f3ef4dd675e97dfc679925acc222b9
xuzewei <microprograms@126.com>
2019-11-15 00:20:57 +0800

    3.4.0

--- src/main/java/com/github/microprograms/micro_oss_core/TransactionExecuter.java
+++
@@ -1,8 +0,0 @@
-package com.github.microprograms.micro_oss_core;
-
-import com.github.microprograms.micro_oss_core.exception.MicroOssException;
-
-@FunctionalInterface
-public interface Transaction {
-	void execute(MicroOssProvider microOssProvider) throws MicroOssException;
-}
