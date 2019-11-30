3de2d0e402edc214b9a92f6e8b983d4d7284ff98
xuzewei <microprograms@126.com>
2019-11-30 15:28:21 +0800

    10.0.0

--- src/main/java/com/github/microprograms/micro_oss_core/QueryResult.java
+++ src/main/java/com/github/microprograms/micro_oss_core/QueryResult.java
@@ -8,7 +8,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.github.microprograms.micro_oss_core.model.Entity;
 
-public class QueryResult<T extends Object> {
+public class QueryResult<T> {
 	private List<Entity> entities;
 	private Class<T> clz;
 
